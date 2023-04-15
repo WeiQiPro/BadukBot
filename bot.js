8252249822c52d793fb9373a3005453f7c90250c
WeiQiPro <108106416+WeiQiPro@users.noreply.github.com>
2023-04-15 09:11:36 -0400

    ESLint edit

--- bot.js
+++ bot.js
@@ -1,27 +1,25 @@
-import { hourlyLiveStreamNotifications, getNotificationChannelsFromJson} from "./src/live.js";
-import { receiveCommandChallenge } from "./src/challenge.js";
-import { receiveCommandTsumego } from "./src/tsumego.js";
-import { receiveCommandBattle } from "./src/battle.js";
-import { receiveCommandRemove } from "./src/remove.js";
-// import { receiveCommandSgf } from './src/sgf.js';
-import { receiveCommandHere } from "./src/here.js";
-import { receiveCommandAdd } from "./src/add.js";
+import { hourlyLiveStreamNotifications, getNotificationChannelsFromJson } from './src/live.js'
+import { receiveCommandChallenge } from './src/challenge.js'
+import { receiveCommandTsumego } from './src/tsumego.js'
+import { receiveCommandBattle } from './src/battle.js'
+import { receiveCommandRemove } from './src/remove.js'
+// import { receiveCommandSgf } from './src/sgf.js'
+import { receiveCommandHere } from './src/here.js'
+import { receiveCommandAdd } from './src/add.js'
 
-import pkg from "discord.js";
-import fs from "fs";
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: "./database/.env" });
+import pkg from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config({ path: './database/.env' })
 
-const { TOKEN, PREFIX, YOUTUBE_API_KEY } = process.env;
-const { Client, GatewayIntentBits, Permissions, PermissionsBitField } = pkg;
+const { TOKEN, PREFIX } = process.env
+const { Client, GatewayIntentBits, PermissionsBitField } = pkg
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
+    GatewayIntentBits.MessageContent
+  ]
+})
 
 const returnCommandList = () => {
   return `**Commands List**\n
@@ -31,139 +29,140 @@ const returnCommandList = () => {
         example: !tsumego 1k | !tsumego q-123456 \n
   **!battle** - This command send a message to participating servers:
         example: !battle 10 https://pk.101weiqi.com/not_a_real_link \n
-  `;
-};
+  `
+}
 
-client.login(TOKEN);
+client.login(TOKEN)
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
 
-  let scheduledVideosToPost = [];
-  hourlyLiveStreamNotifications(client, scheduledVideosToPost);
+  const scheduledVideosToPost = []
+
+  hourlyLiveStreamNotifications(client, scheduledVideosToPost)
 
   // Calculate time remaining until the next hour
-  const now = new Date();
+  const now = new Date()
   const millisecondsUntilNextHour =
     60 * 60 * 1000 -
     (now.getMinutes() * 60 * 1000 +
       now.getSeconds() * 1000 +
-      now.getMilliseconds());
+      now.getMilliseconds())
 
   // Set a timeout to start the hourly interval on the hour
   setTimeout(() => {
     // Call hourlyLiveNotifications every hour
     setInterval(() => {
-      hourlyLiveStreamNotifications(client, scheduledVideosToPost);
-    }, 60 * 60 * 1000);
+      hourlyLiveStreamNotifications(client, scheduledVideosToPost)
+    }, 60 * 60 * 1000)
 
     // Call hourlyLiveNotifications for the first time on the hour
-    hourlyLiveStreamNotifications(client, scheduledVideosToPost);
-  }, millisecondsUntilNextHour);
+    hourlyLiveStreamNotifications(client, scheduledVideosToPost)
+  }, millisecondsUntilNextHour)
 
   // Check for scheduled videos every 10 minute
-  setInterval(() => {
-    const now = new Date();
-    const tenMinutesBefore = new Date(now.getTime() + 10 * 60 * 1000);
+  setInterval(async () => {
+    const now = new Date()
+    const tenMinutesBefore = new Date(now.getTime() + 10 * 60 * 1000)
+    const guildsNotificationChannels = await getNotificationChannelsFromJson()
 
     for (let i = scheduledVideosToPost.length - 1; i >= 0; i--) {
-      const scheduledVideo = scheduledVideosToPost[i];
+      const scheduledVideo = scheduledVideosToPost[i]
       const videoScheduledStartTime = new Date(
         scheduledVideo.videoScheduledStartTime
-      );
+      )
 
       if (videoScheduledStartTime <= tenMinutesBefore) {
         // Post the scheduled video notification
-        const message = `${scheduledVideo.channelName} will be live in 10 minutes!\n${scheduledVideo.videoTitle}\n${scheduledVideo.videoURL}`;
-        const guildsNotificationChannels = getNotificationChannelsFromJson()
-        guildsNotificationChannels.forEach((channel => {
-
+        const message = `${scheduledVideo.channelName} will be live in 10 minutes!\n${scheduledVideo.videoTitle}\n${scheduledVideo.videoURL}`
+        guildsNotificationChannels.forEach((channel) => {
           try {
-            client.channels.cache.get(channel).send(message);
-            console.log(`Posted ${message} to ${channel}`);
+            client.channels.cache.get(channel).send(message)
+            console.log(`Posted ${message} to ${channel}`)
           } catch (error) {
-            console.error(`Channel with ID ${channel} not found in cache.`);
+            console.error(
+              `Error posting message to channel ${channel}: ${error}`
+            )
+            console.error(error.stack)
           }
-        }
-        ));
+        })
 
         // Remove the scheduled video from the list
-        scheduledVideosToPost.splice(i, 1);
+        scheduledVideosToPost.splice(i, 1)
       }
     }
-  }, 10 * 60 * 1000);
-});
+  }, 10 * 60 * 1000)
+})
 
-client.on("messageCreate", (discordMessageStack) => {
+client.on('messageCreate', (discordMessageStack) => {
   if (
     discordMessageStack.author.bot ||
     !discordMessageStack.content.toLowerCase().startsWith(PREFIX)
-  )
-    return;
+  ) {
+    return
+  }
 
   const args = discordMessageStack.content
     .slice(PREFIX.length)
     .trim()
-    .split(/ +/);
-  const command = args.shift().toLowerCase();
+    .split(/ +/)
+  const command = args.shift().toLowerCase()
   if (command) {
-    readUserSentCommand(command, args, discordMessageStack);
+    readUserSentCommand(command, args, discordMessageStack)
   }
-});
+})
 
-function readUserSentCommand(
+function readUserSentCommand (
   commandSentByUser,
   userMessageContent,
   messageStack
 ) {
   const doesUserHavePermission = messageStack.member.permissions.has([
     PermissionsBitField.Flags.KickMembers,
-    PermissionsBitField.Flags.BanMembers,
+    PermissionsBitField.Flags.BanMembers
   ])
-    ? true
-    : false;
 
-  const Tangjie = "128781081292832768";
+  const Tangjie = '128781081292832768'
 
   switch (commandSentByUser) {
-    case "add":
+    case 'add':
       if (messageStack.author.id === Tangjie) {
-        receiveCommandAdd(userMessageContent, messageStack);
+        receiveCommandAdd(userMessageContent, messageStack)
       } else {
-        messageStack.channel.send("You do not have permission to use this.");
+        messageStack.channel.send('You do not have permission to use this.')
       }
-      break;
-    case "remove":
+      break
+    case 'remove':
       if (messageStack.author.id === Tangjie) {
-        receiveCommandRemove(userMessageContent, messageStack);
+        receiveCommandRemove(userMessageContent, messageStack)
       } else {
-        messageStack.channel.send("You do not have permission to use this.");
+        messageStack.channel.send('You do not have permission to use this.')
       }
-      break;
-    case "tsumego":
-      receiveCommandTsumego(userMessageContent, messageStack);
-      break;
-    case "battle":
-      receiveCommandBattle(userMessageContent, messageStack, client);
-      break;
-    case "sgf":
-      receiveCommandSGF(userMessageContent, messageStack);
-      break;
-    case "challenge":
-      receiveCommandChallenge(userMessageContent, messageStack);
-      break;
-    case "here":
+      break
+    case 'tsumego':
+      receiveCommandTsumego(userMessageContent, messageStack)
+      break
+    case 'battle':
+      receiveCommandBattle(userMessageContent, messageStack, client)
+      break
+    case 'sgf':
+      // receiveCommandSGF(userMessageContent, messageStack)
+      break
+    case 'challenge':
+      receiveCommandChallenge(userMessageContent, messageStack)
+      break
+    case 'here':
       if (doesUserHavePermission) {
-        receiveCommandHere(userMessageContent, messageStack);
+        receiveCommandHere(userMessageContent, messageStack)
       } else {
-        messageStack.channel.send("You do not have permission to use this.");
+        messageStack.channel.send('You do not have permission to use this.')
       }
-      break;
-    case "help":
-      messageStack.channel.send(returnCommandList());
-      break;
+      break
+    case 'help':
+      messageStack.channel.send(returnCommandList())
+      break
     default:
-      messageStack.channel.send("Command not found.");
-      break;
+      messageStack.channel.send('Command not found.')
+      break
   }
 }
--- src/live.js
+++ src/live.js
@@ -1,182 +1,182 @@
-import fs from "fs";
-import fetch from "node-fetch";
-import { parse } from "node-html-parser";
-import { google } from "googleapis";
+import fs from 'fs'
+import fetch from 'node-fetch'
+import { parse } from 'node-html-parser'
+import { google } from 'googleapis'
 
-import dotenv from "dotenv";
-dotenv.config({ path: "./database/.env" });
+import dotenv from 'dotenv'
+dotenv.config({ path: './database/.env' })
 
 const youtube = google.youtube({
-  version: "v3",
-  auth: process.env.YOUTUBE_API_KEY,
-});
+  version: 'v3',
+  auth: process.env.YOUTUBE_API_KEY
+})
 
-export async function hourlyLiveStreamNotifications(
+export async function hourlyLiveStreamNotifications (
   client,
   scheduledVideosToPost
 ) {
   try {
-    const now = new Date();
-    const notificationChannels = await getNotificationChannelsFromJson();
-    const liveStreamVideos = await getLiveStreamsVideos();
+    const now = new Date()
+    const notificationChannels = await getNotificationChannelsFromJson()
+    const liveStreamVideos = await getLiveStreamsVideos()
 
     for (let i = liveStreamVideos.length - 1; i >= 0; i--) {
-      const scheduledVideo = liveStreamVideos[i];
+      const scheduledVideo = liveStreamVideos[i]
 
-      if (scheduledVideo.videoScheduledStartTime == "Invalid Date") {
-        scheduledVideo.videoScheduledStartTime = new Date() - 60 * 60 * 1000;
+      if (scheduledVideo.videoScheduledStartTime === 'Invalid Date') {
+        scheduledVideo.videoScheduledStartTime = new Date() - 60 * 60 * 1000
       }
 
       const videoScheduledStartTime = new Date(
         scheduledVideo.videoScheduledStartTime
-      );
+      )
       if (videoScheduledStartTime > now) {
         if (
           scheduledVideosToPost.some(
             (video) => video.videoURL === scheduledVideo.videoURL
           )
         ) {
-          liveStreamVideos.splice(i, 1);
-          continue;
+          liveStreamVideos.splice(i, 1)
+          continue
         } else if (videoScheduledStartTime - now < 60 * 60 * 1000) {
-          scheduledVideosToPost.push(scheduledVideo);
-          liveStreamVideos.splice(i, 1);
+          scheduledVideosToPost.push(scheduledVideo)
+          liveStreamVideos.splice(i, 1)
         } else {
-          liveStreamVideos.splice(i, 1);
+          liveStreamVideos.splice(i, 1)
         }
       }
     }
 
-    postLiveVideosToChannels(liveStreamVideos, notificationChannels, client);
+    postLiveVideosToChannels(liveStreamVideos, notificationChannels, client)
   } catch (error) {
-    console.error(`Error in hourlyLiveStreamNotifications: ${error}`);
+    console.error(`Error in hourlyLiveStreamNotifications: ${error}`)
   }
 }
 
-export async function postLiveVideosToChannels(
+export async function postLiveVideosToChannels (
   liveStreamVideos,
   notificationChannels,
   client
 ) {
   for (const liveStreamVideo of liveStreamVideos) {
-    const message = `${liveStreamVideo.channelName} is live!\n${liveStreamVideo.videoTitle}\n${liveStreamVideo.videoURL}`;
+    const message = `${liveStreamVideo.channelName} is live!\n${liveStreamVideo.videoTitle}\n${liveStreamVideo.videoURL}`
     for (const channel of notificationChannels) {
       const messages = await client.channels.cache
         .get(channel)
-        .messages.fetch({ limit: 25 });
+        .messages.fetch({ limit: 25 })
       const messageWithUrl = messages.find((msg) =>
         msg.content.includes(liveStreamVideo.videoURL)
-      );
+      )
       if (!messageWithUrl) {
-        client.channels.cache.get(channel).send(message);
-        console.log(`Posted ${message} to ${channel}`);
+        client.channels.cache.get(channel).send(message)
+        console.log(`Posted ${message} to ${channel}`)
       }
     }
   }
 }
 
-export async function getNotificationChannelsFromJson() {
-  const guildsData = JSON.parse(fs.readFileSync("./database/channels.json"));
-  const guilds = guildsData.guilds;
-  const notificationChannels = [];
+export async function getNotificationChannelsFromJson () {
+  const guildsData = JSON.parse(fs.readFileSync('./database/channels.json'))
+  const guilds = guildsData.guilds
+  const notificationChannels = []
 
   for (const guild of guilds) {
     if (guild.channels && guild.channels.notifications) {
-      notificationChannels.push(guild.channels.notifications.id);
+      notificationChannels.push(guild.channels.notifications.id)
     }
   }
 
-  return notificationChannels;
+  return notificationChannels
 }
 
-async function getLiveStreamsVideos() {
+async function getLiveStreamsVideos () {
   const youtubeChannels = JSON.parse(
-    fs.readFileSync("./database/youtube.json")
-  );
-  const youtubeLiveStreams = await getLiveStreamsFromYoutube(youtubeChannels);
+    fs.readFileSync('./database/youtube.json')
+  )
+  const youtubeLiveStreams = await getLiveStreamsFromYoutube(youtubeChannels)
 
-  return youtubeLiveStreams;
+  return youtubeLiveStreams
 }
 
-async function getYoutubeLiveStreams(channelIds) {
-  const liveStreams = [];
+async function getYoutubeLiveStreams (channelIds) {
+  const liveStreams = []
 
   for (const channelId of channelIds) {
-    const { isStreaming, canonicalURL } = await isChannelStreaming(channelId);
-    if (isStreaming != false && canonicalURL != "") {
-      const videoId = canonicalURL.split("/watch?v=")[1];
-      const videoDetails = await getVideoDetails(videoId);
+    const { isStreaming, canonicalURL } = await isChannelStreaming(channelId)
+    if (isStreaming !== false && canonicalURL !== '') {
+      const videoId = canonicalURL.split('/watch?v=')[1]
+      const videoDetails = await getVideoDetails(videoId)
       const scheduledStartTime = new Date(
         videoDetails.liveStreamingDetails.scheduledStartTime
-      );
-      const videoTitle = videoDetails.snippet.title;
+      )
+      const videoTitle = videoDetails.snippet.title
 
       liveStreams.push({
         title: videoTitle,
         url: `https://www.youtube.com/watch?v=${videoId}`,
-        scheduledStartTime,
-      });
+        scheduledStartTime
+      })
     }
   }
 
-  return liveStreams;
+  return liveStreams
 }
 
-async function isChannelStreaming(channelId) {
+async function isChannelStreaming (channelId) {
   try {
     const response = await fetch(
       `https://youtube.com/channel/${channelId}/live`
-    );
-    const text = await response.text();
-    const html = parse(text);
-    const canonicalURLTag = html.querySelector("link[rel=canonical]");
+    )
+    const text = await response.text()
+    const html = parse(text)
+    const canonicalURLTag = html.querySelector('link[rel=canonical]')
     const canonicalURL = canonicalURLTag
-      ? canonicalURLTag.getAttribute("href")
-      : "";
-    const isStreaming = canonicalURL.includes("/watch?v=");
+      ? canonicalURLTag.getAttribute('href')
+      : ''
+    const isStreaming = canonicalURL.includes('/watch?v=')
 
-    return { isStreaming, canonicalURL };
+    return { isStreaming, canonicalURL }
   } catch (error) {
-    console.error(`Error in isChannelStreaming: ${error}`);
-    return { isStreaming: false, canonicalURL: "" };
+    console.error(`Error in isChannelStreaming: ${error}`)
+    return { isStreaming: false, canonicalURL: '' }
   }
 }
 
-async function getVideoDetails(videoId) {
+async function getVideoDetails (videoId) {
   try {
     const response = await youtube.videos.list({
-      part: "snippet,liveStreamingDetails",
-      id: videoId,
-    });
+      part: 'snippet,liveStreamingDetails',
+      id: videoId
+    })
 
     if (response.data && response.data.items.length > 0) {
-      return response.data.items[0];
+      return response.data.items[0]
     } else {
-      throw new Error("No video details found.");
+      throw new Error('No video details found.')
     }
   } catch (error) {
-    console.error(`Error in getVideoDetails: ${error}`);
-    return {};
+    console.error(`Error in getVideoDetails: ${error}`)
+    return {}
   }
 }
 
-async function getLiveStreamsFromYoutube(youtubeChannels) {
-  const youtubeLiveStreams = [];
+async function getLiveStreamsFromYoutube (youtubeChannels) {
+  const youtubeLiveStreams = []
 
   for (const channel of youtubeChannels) {
-    const liveVideos = await getYoutubeLiveStreams([channel.id]);
+    const liveVideos = await getYoutubeLiveStreams([channel.id])
     if (liveVideos.length > 0) {
-      const liveVideo = liveVideos[0]; // Assuming there's only one live stream per channel
+      const liveVideo = liveVideos[0] // Assuming there's only one live stream per channel
       const liveStream = {
         channelName: channel.name,
         channelURL: `https://www.youtube.com/${channel.name}`,
         videoTitle: liveVideo.title,
         videoURL: liveVideo.url,
-        videoScheduledStartTime: liveVideo.scheduledStartTime,
-      };
-      youtubeLiveStreams.push(liveStream);
+        videoScheduledStartTime: liveVideo.scheduledStartTime
+      }
+      youtubeLiveStreams.push(liveStream)
     }
   }
 
-  return youtubeLiveStreams;
+  return youtubeLiveStreams
 }
